9d10ef430678f1724a867087a4b290525f16f384
Park JunYong <dyden119@gmail.com>
2024-03-07 23:23:32 +0900

    fix: IDバグ修正

--- src/App.js
+++ src/App.js
@@ -12,12 +12,11 @@ function App() {
     const list = await res.json();
     const item = list.slice(0, 20).map((it) => {
       return {
-        key: it.id++,
         author: it.email,
         content: it.body,
         emotion: Math.floor(Math.random() * 5) + 1,
         created_date: new Date().getTime(),
-        id: it.id,
+        id: dataId.current++,
       };
     });
     setData(item);
