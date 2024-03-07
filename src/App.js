ac037c87c75a94c2aa04211fbfbdf51707670a00
Park JunYong <dyden119@gmail.com>
2024-03-08 00:04:58 +0900

    feat: useMemo使って演算最適化テスト

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useMemo } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
@@ -52,9 +52,23 @@ function App() {
     );
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("일기 분석 시작");
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>全ての日記：{data.length}編</div>
+      <div>気持ち良い日記：{goodCount}編</div>
+      <div>気持ち悪い日記：{badCount}編</div>
+      <div>気持ち良い日記の比率：{Math.floor(goodRatio)}%</div>
       <DiaryList onRemove={onRemove} diaryList={data} onEdit={onEdit} />
     </div>
   );
